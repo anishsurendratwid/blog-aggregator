0640a46b7fa8f8acb288e8232071665aed41fa3e
Anish Surendra <anishsurendra@Anishs-MacBook-Air.local>
2025-02-19 14:30:49 +0530

    added a tags to blogs

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,8 +9,10 @@
     <h1>All Blog Posts</h1>
 
     @foreach ($posts as $post)
-        <div style="max-width: 800px; margin: 20px auto; padding: 20px; border: 1px solid #ccc; background-color: #f9f9f9;">
-            <h2>{{ $post->title }}</h2>
+    <div style="max-width: 800px; margin: 20px auto; padding: 20px; border: 1px solid #ccc; background-color: #f9f9f9;">
+            <a href="{{ url('post/'.$post->id) }}">
+                <h2>{{ $post->title }}</h2>
+            </a>
             <div>
                 <p>{{ $post->content }}</p>
             </div>
